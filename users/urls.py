cec5b3c762ac22c2383753d4df9304febf1407b9
jinseok0212 <qowlstjr123@icloud.com>
2025-05-12 16:52:48 +0900

    인증 토큰 확인, 이전 커밋에서 path충돌 수정

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .views import UserRegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import UserInfoView
 
 urlpatterns = [
 
     path('register/', UserRegisterView.as_view()),
     path('token/', TokenObtainPairView.as_view(), name = 'token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh'),
-
+    path('userinfo/', UserInfoView.as_view(), name = 'user_info'),
 ]
 
 
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserRegisterSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class UserRegisterView(APIView):
     def post(self,request):
@@ -10,3 +11,13 @@ class UserRegisterView(APIView):
             serializer.save()
             return Response({"message" : "회원가입 성공!"}, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+    
+class UserInfoView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        return Response({
+            "username": request.user.username,
+            "nickname": request.user.nickname,
+            "age": request.user.age,
+        })
